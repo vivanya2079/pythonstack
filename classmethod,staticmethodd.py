889ef24228b593fc73e4a9680219f5d1cf09875c
vivanya2079 <146419961+vivanya2079@users.noreply.github.com>
2023-10-07 11:43:07 +0530

    Add files via upload

---
+++ classmethod,staticmethodd.py
@@ -0,0 +1,23 @@
+# classmethod
+
+# class MyClass:
+#     def __init__(self, value):
+#         self.value = value
+#     def get_value(self):
+#         return self.value
+# obj = MyClass(10)
+# print(obj.get_value())
+
+
+
+# staticmethod
+
+# class MyClass:
+#     def __init__(self, value):
+#         self.value = value
+#     @staticmethod
+#     def get_max_value(x, y):
+#         return max(x, y)
+# obj = MyClass(10)
+# print(MyClass.get_max_value(20, 30))  
+# print(obj.get_max_value(20, 30)) 
