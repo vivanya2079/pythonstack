49d75fe54ec2c83fc01b4b9efa28da378205df9d
vivanya2079 <146419961+vivanya2079@users.noreply.github.com>
2023-10-09 11:35:01 +0530

    Add files via upload

---
+++ calendar&datetime.py
@@ -0,0 +1,41 @@
+# calendar
+
+# import calendar
+# yy=2023
+# mm=9
+# print(calendar.month(yy,mm))
+
+
+# dates
+
+# import datetime
+# x=datetime.datetime.now()
+# print(x)
+
+
+# import datetime
+# x=datetime.datetime.now()
+# print(x.year)
+# print(x.strftime("%A"))
+
+
+# import datetime
+# x=datetime.datetime(2020,5,17)
+# print(x)
+
+# import datetime
+# x=datetime.datetime(2023,4,1)
+# print(x.strftime("%B"))
+
+
+# random
+
+# import random
+# list1=[1,2,3,4,5,6]
+# print(random.choice(list1))
+
+
+# math
+
+# import math
+# print(math.pi)
