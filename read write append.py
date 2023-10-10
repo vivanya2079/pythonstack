d9127b0e05ec751bd4dff8235c406dd2c24a5fa4
vivanya2079 <146419961+vivanya2079@users.noreply.github.com>
2023-10-10 20:57:45 +0530

    Add files via upload

---
+++ read write append.py
@@ -0,0 +1,45 @@
+# read
+
+# f=open("python.txt","r")
+# print(f.read())
+
+# print(f.read(2))
+
+# print(f.readline())
+
+# print(f.readlines())
+
+# print(f.readline(3))
+
+# print(f.readlines(3))
+
+
+# f=open("C:/Users/vivan/OneDrive/Documents/samples.py","r")
+# print(f.read())
+
+# f.close()
+
+
+# write
+
+# f=open("test.txt","w")
+# print(f.write("python is a high level programming language"))
+
+# f=open("C:/Users/vivan/OneDrive/Documents/python1.py","w")
+# print(f.write("python is a high level programming language"))
+
+
+# append
+
+# f=open("C:/Users/vivan/OneDrive/Documents/python1.py","a")
+# print(f.write("hello vivanyaaa where are you\n vrindha\n swapna"))
+# f.close()
+
+
+# f=open("test.txt","a")
+# print(f.write("hello vivanyaaa where are you\n vrindha\n swapna"))
+
+
+
+
+
---
+++ with statement.py
@@ -0,0 +1,10 @@
+# with open("with.txt") as file:
+#     print(file.read())
+    
+
+# with open("with.txt","w") as file:
+    # print(file.write("hello python"))
+
+
+# with open("with.txt","a") as file:
+    # print(file.write("hello vivanyaah\n vrinda\n swapna"))
