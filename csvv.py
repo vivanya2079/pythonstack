3e8982c483432f67f0fab983f35d65e3f83c5c29
vivanya2079 <146419961+vivanya2079@users.noreply.github.com>
2023-10-13 20:51:19 +0530

    Create csvv.py

---
+++ csvv.py
@@ -0,0 +1,20 @@
+# import csv
+# file_path="C:/Users/vivan/Music/python programs/new.csv"
+# encoding="utf-8"
+# with open(file_path,"r",encoding=encoding,newline='') as csvfile:
+#         readd = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#         for row in readd:
+#                 print("".join(row))
+                
+                
+# import csv
+# file_path="C:/Users/legal/Downloads/research-and-development-survey-2022-csv-notes.csv"
+# encoding="utf-8"
+# data_to_write = [
+#     ["research", "operations"],
+#     ["value1", "value2"],  # Replace with your actual data
+#     ["value3", "value4"],
+# ]
+# with open(file_path,"w",encoding=encoding,newline='') as csvfile:
+#     csvwriter=csv.writer(csvfile)
+#     csvwriter.writerows(data_to_write)
