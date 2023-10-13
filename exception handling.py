87f4a63d290fb1c25e6a8992b697ff70e919665c
vivanya2079 <146419961+vivanya2079@users.noreply.github.com>
2023-10-13 20:44:58 +0530

    Create exception handling.py

---
+++ exception handling.py
@@ -0,0 +1,29 @@
+# def AbyB(a , b): 
+#     try: 
+#         c = ((a+b) / (a-b)) 
+#     except ZeroDivisionError: 
+#         print ("a/b result in 0") 
+#     else: 
+#         print (c) 
+  
+# # Driver program to test above function 
+# AbyB(4.0, 4.0) 
+# AbyB(5.0, 6.0) 
+
+
+# try: 
+#     k = 5//0
+#     print(k) 
+# except ZeroDivisionError: 
+#     print("Can't divide by zero") 
+# finally: 
+#     print('This is always executed') 
+
+
+
+# a=[1,2,3]
+# try:
+#     print("second element =%d"%(a[1]))
+#     print ("third element = %d"%(a[3]))
+# except: 
+#     print ("An error occurred")
